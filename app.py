619a9f918fb56a94d2167dd3fd5092f4aa8eb59e
Maxwell M. Marques <maxwellmontques@gmail.com>
2024-01-02 14:30:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -16,16 +16,23 @@ def cadastrar():
     new_username = st.text_input("Novo Usuário")
     new_password = st.text_input("Nova Senha", type="password")
     confirm_password = st.text_input("Confirmar Senha", type="password")
+
+    if len(new_username) < 3:
+        st.error("O nome de usuário deve ter no mínimo 3 caracteres.")
+    
+    if len(new_password) < 6:
+        st.error("A senha deve ter no mínimo 6 caracteres.")
     
     if new_password != confirm_password:
         st.error("As senhas não coincidem.")
     
-    if st.button("Sign Up"):
+    if len(new_username) >= 3 and len(new_password) >= 6 and st.button("Sign Up"):
         st.success("Nova conta cadastrada com sucesso!")
+        st.session_state.page = "Login"
     
     if st.button("Login"):
         st.session_state.page = "Login"
-
+        
 def main():
     st.set_page_config(page_title='GADM')
 
@@ -36,6 +43,6 @@ def main():
         login()
     elif st.session_state.page == "Sign Up":
         cadastrar()
-
+        
 if __name__ == "__main__":
     main()

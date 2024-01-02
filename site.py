141aae8636a2390e3e12f8a64978f8eb08dc5333
Maxwell M. Marques <maxwellmontques@gmail.com>
2024-01-02 14:53:37 -0300

    Update site.py

--- site.py
+++ site.py
@@ -3,5 +3,12 @@ import streamlit as st
 def login():
     st.title('Login')
     st.text_input('Usuário')
+    st.text_input('Senha', type='password')
     
-login()
+def sign_up():
+    st.title('Sign Up')
+    st.text_input('Novo Usuário')
+    st.text_input('Senha', type='password')
+    st.text_input('Confirmar Senha', type='password')
+
+sign_up()

5a88ea185c9247164d736d7cddd48da98bf49d34
Maxwell M. Marques <maxwellmontques@gmail.com>
2024-01-02 14:57:39 -0300

    Update site.py

--- site.py
+++ site.py
@@ -4,11 +4,13 @@ def login():
     st.title('Login')
     st.text_input('Usuário')
     st.text_input('Senha', type='password')
+    st.button('Entrar')
     
 def sign_up():
     st.title('Sign Up')
     st.text_input('Novo Usuário')
     st.text_input('Senha', type='password')
     st.text_input('Confirmar Senha', type='password')
+    st.button('Novo Login')
 
 sign_up()

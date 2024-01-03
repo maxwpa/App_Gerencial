c0522cd4c96d5fa2a42af3d6c4b909e9bc59e303
Maxwell M. Marques <maxwellmontques@gmail.com>
2024-01-03 08:50:35 -0300

    Update coleta_de_dados.py

--- coleta_de_dados.py
+++ coleta_de_dados.py
@@ -1,39 +1,45 @@
 import streamlit as st
 
-produto_comprado = st.text_input('Produto Comprado')
-data_da_compra = st.date_input('Data da Compra')
-preco_da_compra = st.number_input('Preço da Compra em Reais', step=0.01, format="%.2f")
-custos_adicionais = st.number_input('Custos Adicionais', step=0.01, format="%.2f")
+codigo_de_acesso = st.text_input('Código de Acesso', type='password')
 
-opcoes_pagamento = ["Boleto Bancário", "PIX", "Dinheiro Vivo", "Cartão de Crédito", "Cartão de Débito"]
-forma_de_pagamento = st.selectbox("Forma de Pagamento", opcoes_pagamento)
+if codigo_de_acesso == 20210088628:
+    produto_comprado = st.text_input('Produto Comprado')
+    data_da_compra = st.date_input('Data da Compra')
+    preco_da_compra = st.number_input('Preço da Compra em Reais', step=0.01, format="%.2f")
+    custos_adicionais = st.number_input('Custos Adicionais', step=0.01, format="%.2f")
 
-pagamento = ['À Vista', 'Parcelamento']
-forma_de_pagamento = st.selectbox("Forma de Pagamento", pagamento)
+    opcoes_pagamento = ["Boleto Bancário", "PIX", "Dinheiro Vivo", "Cartão de Crédito", "Cartão de Débito"]
+    forma_de_pagamento = st.selectbox("Forma de Pagamento", opcoes_pagamento)
 
-if forma_de_pagamento == 'À Vista':
-    parcelamento = 1
-    valor_de_entrada = preco_da_compra
-    valor_das_parcelas = 0
-else:
-    parcelas = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    parcelamento = st.selectbox("Número de Parcelas", parcelas)
-    valor_de_entrada = st.number_input('Valor Pago na Entrada', step=0.01, format="%.2f")
-    valor_das_parcelas = st.number_input('Valor das Parcelas', step=0.01, format="%.2f")
+    pagamento = ['À Vista', 'Parcelamento']
+    forma_de_pagamento = st.selectbox("Forma de Pagamento", pagamento)
 
-data_de_pagamento = st.date_input('Data de Pagamento')
-quantidade_comprada = st.number_input("Quantidade Comprada", step=1, format="%d")
-fornecedor = st.text_input('Fornecedor')
-
-if produto_comprado and data_da_compra and preco_da_compra and custos_adicionais and forma_de_pagamento and data_de_pagamento and quantidade_comprada and fornecedor:
     if forma_de_pagamento == 'À Vista':
-        custo_final = preco_da_compra + custos_adicionais
+        parcelamento = 1
+        valor_de_entrada = preco_da_compra
+        valor_das_parcelas = 0
     else:
-        custo_final = valor_das_parcelas * parcelamento + valor_de_entrada
+        parcelas = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+        parcelamento = st.selectbox("Número de Parcelas", parcelas)
+        valor_de_entrada = st.number_input('Valor Pago na Entrada', step=0.01, format="%.2f")
+        valor_das_parcelas = st.number_input('Valor das Parcelas', step=0.01, format="%.2f")
 
-    if quantidade_comprada != 0:
-        custo_unitario = custo_final / quantidade_comprada
+    data_de_pagamento = st.date_input('Data de Pagamento')
+    quantidade_comprada = st.number_input("Quantidade Comprada", step=1, format="%d")
+    fornecedor = st.text_input('Fornecedor')
 
-    cadastrar_compra = st.button('Cadastrar Compra')
+    if produto_comprado and data_da_compra and preco_da_compra and custos_adicionais and forma_de_pagamento and data_de_pagamento and quantidade_comprada and fornecedor:
+        if forma_de_pagamento == 'À Vista':
+            custo_final = preco_da_compra + custos_adicionais
+        else:
+            custo_final = valor_das_parcelas * parcelamento + valor_de_entrada
+
+        if quantidade_comprada != 0:
+            custo_unitario = custo_final / quantidade_comprada
+
+        cadastrar_compra = st.button('Cadastrar Compra')
+    else:
+        st.warning("Preencha todos os campos em branco antes de cadastrar a compra.")
+        
 else:
-    st.warning("Preencha todos os campos obrigatórios antes de cadastrar a compra.")
+    st.warning("O código de acesso inserido não foi aceito.")

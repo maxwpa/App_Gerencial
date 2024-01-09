7604787ca0ba61c1e9665b603d35960ed4041531
Maxwell M. Marques <maxwellmontques@gmail.com>
2024-01-09 16:20:27 -0300

    Update apk.py

--- apk.py
+++ apk.py
@@ -55,86 +55,88 @@ def acesso():
         st.warning("Código de acesso incorreto. Tente novamente.")
 
 def indice_paginas():
-    st.sidebar.title('Índice de Páginas')
-    
-    paginas = {
-        'Tabela de Compras': tabela,
-        'Registrar Compra': coleta,
-        'DashBoard Compras': dashboard
-    }
-    
-    escolha_pagina = st.sidebar.radio('Selecione a Página', list(paginas.keys()))
-    
-    paginas[escolha_pagina]()
+    if hasattr(st.session_state, 'logged_in') and st.session_state.logged_in:    
+        st.sidebar.title('Índice de Páginas')
+
+        paginas = {
+            'Tabela de Compras': tabela,
+            'Registrar Compra': coleta,
+            'DashBoard Compras': dashboard
+        }
+
+        escolha_pagina = st.sidebar.radio('Selecione a Página', list(paginas.keys()))
+
+        paginas[escolha_pagina]()
 
 def coleta():
-    produto = st.text_input('Produto Comprado').upper()
-    
-    letras = ''.join(random.choices(string.ascii_uppercase, k=2))
-    numeros = ''.join(random.choices(string.digits, k=5))
-    id_produto = letras + numeros
-    
-    tamanho = st.text_input('Tamanho, ex: G, GG ou 36, 38').upper()
-    grupo = ['Masculino', 'Feminino', 'Unissex']
-    genero = st.selectbox('Gênero', grupo)
-    nicho = ['Adulto', 'Infantil']
-    publico = st.selectbox('Público', nicho)
-    quantidade = st.number_input("Quantidade Comprada", step=1, format="%d")
-    data_compra = st.date_input('Data da Compra')
-    preco = st.number_input('Preço da Compra em Reais', step=0.01, format="%.2f")
-    custos_adicionais = st.number_input('Custos Adicionais', step=0.01, format="%.2f")
+    if hasattr(st.session_state, 'logged_in') and st.session_state.logged_in:
+        produto = st.text_input('Produto Comprado').upper()
 
-    opcoes_pagamento = ["Boleto Bancário", "PIX", "Dinheiro Vivo", "Cartão de Crédito", "Cartão de Débito"]
-    forma_de_pagamento = st.selectbox("Forma de Pagamento", opcoes_pagamento)
-    
-    paga = ['À Vista', 'Parcelado']
-    pagamento = st.selectbox("Opções de Pagamento", paga)
-     
-    if pagamento == 'À Vista':
-        parcelamento = 0
-        valor_entrada = preco
-        valor_parcela = 0
-    else:
-        parcelas = list(range(1, 13))
-        parcelamento = st.selectbox("Número de Parcelas", parcelas)
-        valor_entrada = st.number_input('Valor Pago na Entrada', step=0.01, format="%.2f")
-        valor_parcela = st.number_input('Valor das Parcelas', step=0.01, format="%.2f")
+        letras = ''.join(random.choices(string.ascii_uppercase, k=2))
+        numeros = ''.join(random.choices(string.digits, k=5))
+        id_produto = letras + numeros
 
-    data_pagamento = st.date_input('Data de Pagamento (à vista ou última parcela)')
+        tamanho = st.text_input('Tamanho, ex: G, GG ou 36, 38').upper()
+        grupo = ['Masculino', 'Feminino', 'Unissex']
+        genero = st.selectbox('Gênero', grupo)
+        nicho = ['Adulto', 'Infantil']
+        publico = st.selectbox('Público', nicho)
+        quantidade = st.number_input("Quantidade Comprada", step=1, format="%d")
+        data_compra = st.date_input('Data da Compra')
+        preco = st.number_input('Preço da Compra em Reais', step=0.01, format="%.2f")
+        custos_adicionais = st.number_input('Custos Adicionais', step=0.01, format="%.2f")
 
-    fornecedor = st.text_input('Fornecedor').upper()
-    
-    data_entrega = st.date_input('Prazo de Entrega')
-    
-    preco = round(preco, 2)
-    custos_adicionais = round(custos_adicionais, 2)
+        opcoes_pagamento = ["Boleto Bancário", "PIX", "Dinheiro Vivo", "Cartão de Crédito", "Cartão de Débito"]
+        forma_de_pagamento = st.selectbox("Forma de Pagamento", opcoes_pagamento)
 
-    if quantidade > 0:
-        custo_unitario = (preco + custos_adicionais) / quantidade
-    else:
-        custo_unitario = 0
+        paga = ['À Vista', 'Parcelado']
+        pagamento = st.selectbox("Opções de Pagamento", paga)
 
-    
-    if pagamento == 'À Vista':
-        custo_final = preco + custos_adicionais
-    else:
-        custo_final = valor_parcela * parcelamento + valor_entrada + custos_adicionais
-
-   
-    custo_unitario = round(custo_unitario, 2)
-    custo_final = round(custo_final, 2)
-
-    campos_preenchidos = produto and data_compra and preco and custos_adicionais and forma_de_pagamento and data_pagamento and quantidade and fornecedor and data_entrega
-
-    registrar_compra = st.button('Registrar Compra', disabled=not campos_preenchidos)
-    
-    if registrar_compra:
-        if campos_preenchidos:
-            inserir_dados(id_produto, produto, tamanho, genero, publico, quantidade, data_compra, preco, custos_adicionais, pagamento, forma_de_pagamento, parcelamento, valor_entrada, valor_parcela, data_pagamento, fornecedor, data_entrega, custo_unitario, custo_final)
-
-            st.success("Compra cadastrada com sucesso!")
+        if pagamento == 'À Vista':
+            parcelamento = 0
+            valor_entrada = preco
+            valor_parcela = 0
         else:
-            st.warning("Preencha todos os campos em branco antes de cadastrar a compra.")
+            parcelas = list(range(1, 13))
+            parcelamento = st.selectbox("Número de Parcelas", parcelas)
+            valor_entrada = st.number_input('Valor Pago na Entrada', step=0.01, format="%.2f")
+            valor_parcela = st.number_input('Valor das Parcelas', step=0.01, format="%.2f")
+
+        data_pagamento = st.date_input('Data de Pagamento (à vista ou última parcela)')
+
+        fornecedor = st.text_input('Fornecedor').upper()
+
+        data_entrega = st.date_input('Prazo de Entrega')
+
+        preco = round(preco, 2)
+        custos_adicionais = round(custos_adicionais, 2)
+
+        if quantidade > 0:
+            custo_unitario = (preco + custos_adicionais) / quantidade
+        else:
+            custo_unitario = 0
+
+
+        if pagamento == 'À Vista':
+            custo_final = preco + custos_adicionais
+        else:
+            custo_final = valor_parcela * parcelamento + valor_entrada + custos_adicionais
+
+
+        custo_unitario = round(custo_unitario, 2)
+        custo_final = round(custo_final, 2)
+
+        campos_preenchidos = produto and data_compra and preco and custos_adicionais and forma_de_pagamento and data_pagamento and quantidade and fornecedor and data_entrega
+
+        registrar_compra = st.button('Registrar Compra', disabled=not campos_preenchidos)
+
+        if registrar_compra:
+            if campos_preenchidos:
+                inserir_dados(id_produto, produto, tamanho, genero, publico, quantidade, data_compra, preco, custos_adicionais, pagamento, forma_de_pagamento, parcelamento, valor_entrada, valor_parcela, data_pagamento, fornecedor, data_entrega, custo_unitario, custo_final)
+
+                st.success("Compra cadastrada com sucesso!")
+            else:
+                st.warning("Preencha todos os campos em branco antes de cadastrar a compra.")
             
 def criar_dataframe():
     cursor.execute('SELECT * FROM compras')
@@ -147,8 +149,6 @@ def tabela():
     if hasattr(st.session_state, 'logged_in') and st.session_state.logged_in:
         df_compras = criar_dataframe()
         st.dataframe(df_compras)
-        filtro_genero = st.sidebar.multiselect("Filtrar por Gênero",
-                                               df_compras["genero"])
         
 def dashboard():
     if hasattr(st.session_state, 'logged_in') and st.session_state.logged_in:

b1b881b7feec4aa47062203392e07824d0983267
Maxwell M. Marques <maxwellmontques@gmail.com>
2024-01-10 16:21:10 -0300

    Update apk.py

--- apk.py
+++ apk.py
@@ -242,42 +242,41 @@ def dashboard():
                 unsafe_allow_html=True)
 
         chart_html = f"""
-                        <canvas id="pieChart" width="400" height="400"></canvas>
-                        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-                        <script>
-                        var ctx = document.getElementById('pieChart').getContext('2d');
-                        var myPieChart = new Chart(ctx, {{
-                            type: 'pie',
-                            data: {{
-                                labels: {labels},
-                                datasets: [{{
-                                    data: {data},
-                                    backgroundColor: [
-                                        'rgba(255, 99, 132, 0.7)',
-                                        'rgba(54, 162, 235, 0.7)',
-                                        'rgba(255, 206, 86, 0.7)',
-                                    ],
-                                    borderColor: [
-                                        'rgba(255, 99, 132, 1)',
-                                        'rgba(54, 162, 235, 1)',
-                                        'rgba(255, 206, 86, 1)',
-                                    ],
-                                    borderWidth: 1
-                                }}]
-                            }},
-                            options: {{
-                                title: {{
-                                    display: true,
-                                    text: 'Gastos por Produto'
-                                }}
-                            }}
-                        }});
-                        </script>
-                        """
+<canvas id="pieChart" width="400" height="400"></canvas>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+var ctx = document.getElementById('pieChart').getContext('2d');
+var myPieChart = new Chart(ctx, {{
+    type: 'pie',
+    data: {{
+        labels: {labels},
+        datasets: [{{
+            data: {data},
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.7)',
+                'rgba(54, 162, 235, 0.7)',
+                'rgba(255, 206, 86, 0.7)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+            ],
+            borderWidth: 1
+        }}]
+    }},
+    options: {{
+        title: {{
+            display: true,
+            text: 'Gastos por Produto'
+        }}
+    }}
+}});
+</script>
+"""
 
        
         st.markdown(chart_html, unsafe_allow_html=True)
-        st.write(f"Gasto Total com Compras: R${gasto_total}")
 
 conn.commit()
 

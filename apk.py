857e95705e413a77ae5efa4c08ced0a068828261
Maxwell M. Marques <maxwellmontques@gmail.com>
2024-01-15 18:36:25 -0300

    Update apk.py

--- apk.py
+++ apk.py
@@ -11,7 +11,6 @@ from dateutil.relativedelta import relativedelta
 #from matplotlib.ticker import FuncFormatter
 from streamlit import components
 import numpy as np
-from bokeh.plotting import figure, show
 
 
 

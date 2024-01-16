41115a1ea795c3a5c6f54e37a1b660c6c7090fbb
Maxwell M. Marques <maxwellmontques@gmail.com>
2024-01-16 08:23:37 -0300

    Update apk.py

--- apk.py
+++ apk.py
@@ -676,7 +676,7 @@ def dashboard():
         with col_a2:
             st.markdown(
                 f"""
-                    <div style="border: 3px solid #e2e2e2; border-radius: 5px; padding: 1px; text-align: center; width: 140px; height: 50px; font-family: 'Arial', sans-serif; background-color: #556B2F; box-shadow: inset 0 0 40px rgba(0, 0, 0, 1)">
+                    <div style="border: 3px dotted #e2e2e2; border-radius: 5px; padding: 1px; text-align: center; width: 140px; height: 50px; font-family: 'Arial', sans-serif; background-color: #556B2F; box-shadow: inset 0 0 40px rgba(0, 0, 0, 1)">
                         <h2 style="color: #7FFFD4; font-size: 12px; font-weight: bold; margin-bottom: -25px; margin-top: -18px;">Total Pago</h2>
                         <h1 style="color: #F5F5DC; font-size: 25px; font-weight: normal; margin-top: -38px;">R${contas_pagas:.2f}</h1>
                     </div>
